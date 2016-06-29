1510fa198ffca73b4fe008a92ed7be77b4b025b2
Nick Uraltsev <nick.uraltsev@gmail.com>
2016-06-29 13:27:29 -0700

    Use DefinePlugin to define NODE_ENV

--- client/webpack.config.babel.js
+++ client/webpack.config.babel.js
@@ -30,6 +30,11 @@ export default {
     ],
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
     new ExtractTextPlugin('bundle.css'),
   ],
 };
