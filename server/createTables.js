514c2281344790a6806affda82c443f8b2803c7f
Nick Uraltsev <nick.uraltsev@gmail.com>
2016-09-08 22:00:45 -0700

    Update scripts for creating database tables

---
+++ server/createTables.js
@@ -0,0 +1,13 @@
+import db from './db';
+
+const force = process.argv.slice(2).includes('--force');
+
+async function createTables() {
+  try {
+    await db.sequelize.sync({ force });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+createTables();
