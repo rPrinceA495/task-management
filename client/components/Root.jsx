c2d5bfa07f162221d42504bb9e4b7fcff360fb4a
Nick Uraltsev <nick.uraltsev@gmail.com>
2016-10-27 21:43:24 -0700

    Get rid of _k query string parameter

--- client/components/Root.jsx
+++ client/components/Root.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import { observer, Provider } from 'mobx-react';
-import { Router, Route, IndexRedirect, hashHistory } from 'react-router';
+import { Router, Route, IndexRedirect, useRouterHistory } from 'react-router';
+import { createHashHistory } from 'history';
 import Filters from '../constants/Filters';
 import App from './App.jsx';
 import Projects from './Projects.jsx';
 
+const history = useRouterHistory(createHashHistory)({ queryKey: false });
 const Root = observer(props =>
   <Provider
     projectStore={props.projectStore}
     notificationStore={props.notificationStore}>
-    <Router history={hashHistory}>
+    <Router history={history}>
       <Route path="/" component={App}>
         <IndexRedirect to="projects" />
         <Route path="projects">
